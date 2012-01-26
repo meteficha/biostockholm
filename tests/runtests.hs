6c5946ffd561dd0dd3fc0b5b726f4f3ae3329b90
Felipe Lessa <felipe.lessa@gmail.com>
2012-01-26 09:28:07 +0000

    Add a simple QuickCheck property and many Arbitrary instances.

--- tests/runtests.hs
+++ tests/runtests.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- from base
+import Control.Applicative
+import System.IO.Unsafe (unsafePerformIO)
+
 -- from bytestring
+import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as L
 
 -- from biocore
@@ -10,13 +15,19 @@ import Bio.Core.Sequence
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 
+-- from QuickCheck
+import Test.QuickCheck
+
 -- from hspec
 import Test.Hspec.Monadic
 import Test.Hspec.HUnit ()
+import Test.Hspec.QuickCheck (prop)
 import Test.HUnit
 
 -- from this package
 import Bio.Sequence.Stockholm
+import Bio.Sequence.Stockholm.Document
+import Bio.Sequence.Stockholm.Stream
 
 
 main :: IO ()
@@ -39,6 +50,11 @@ main =
         rendered <- strictRender result2
         again    <- strictParse  rendered
         again @?= result2
+      prop "passes QuickCheck property" $ \sto ->
+        unsafePerformIO $ do
+          rendered <- strictRender sto
+          again    <- strictParse  rendered
+          return (again == sto)
 
 
 ----------------------------------------------------------------------
@@ -103,3 +119,66 @@ strictRender stos = fmap L.fromChunks $
                       CL.sourceList stos C.$=
                       renderStockholm C.$$
                       CL.consume
+
+
+----------------------------------------------------------------------
+
+
+instance Arbitrary Event where
+    arbitrary = frequency
+      [ (1,  pure EvHeader)
+      , (1,  pure EvEnd)
+      , (3,  EvComment <$> arbitrary)
+      , (10, EvSeqData <$> arbitrary <*> arbitrary)
+      , (2,  EvGF      <$> arbitrary <*> arbitrary)
+      , (2,  EvGC      <$> arbitrary <*> arbitrary)
+      , (2,  EvGS      <$> arbitrary <*> arbitrary <*> arbitrary)
+      , (2,  EvGR      <$> arbitrary <*> arbitrary <*> arbitrary)
+      ]
+
+instance Arbitrary Stockholm where
+    arbitrary = Stockholm <$> arbitrary <*> arbitrary <*> arbitrary
+    shrink (Stockholm fileanns clmnanns stseqs) =
+        Stockholm <$> shrink fileanns <*> shrink clmnanns <*> shrink stseqs
+
+
+instance Arbitrary StockholmSeq where
+    arbitrary = StSeq <$> (SeqLabel <$> arbitrary)
+                      <*> arbitrary
+                      <*> arbitrary
+                      <*> arbitrary
+    shrink (StSeq label data_ seqanns clmnanns) =
+        StSeq label data_ <$> shrink seqanns <*> shrink clmnanns
+
+instance Arbitrary d => Arbitrary (Ann d) where
+    arbitrary = Ann <$> arbitrary <*> arbitrary
+
+instance Arbitrary FileAnnotation where
+    arbitrary = annArbitraryHelper list F_Other
+        where
+          list = [ AC, ID, DE, AU, SE, GA, TC, NC, TP, SQ, AM, DC
+                 , DR, RC, RN, RM, RT, RA, RL, PI, KW, CC, NE, NL ]
+
+instance Arbitrary (ColumnAnnotation a) where
+    arbitrary = annArbitraryHelper list C_Other
+        where
+          list = [SS, SA, TM, PP, LI, AS, PAS, SAS, IN]
+
+instance Arbitrary SequenceAnnotation where
+    arbitrary = annArbitraryHelper list S_Other
+        where
+          list = [S_AC, S_DE, S_DR, OS, OC, LO]
+
+annArbitraryHelper :: Arbitrary b => [a] -> (b -> a) -> Gen a
+annArbitraryHelper list other =
+  frequency $ (1, other <$> arbitrary) :
+              [(5, pure x) | x <- list]
+
+instance Arbitrary SeqData where
+    arbitrary = SeqData . L.pack <$> listOf1 (elements ['A', 'T', 'C', 'G'])
+
+instance Arbitrary B.ByteString where
+    arbitrary = B.pack <$> listOf1 (elements $ ['a'..'z'] ++ ['A'..'Z'] ++ " .?!|:[]{}")
+
+instance Arbitrary L.ByteString where
+    arbitrary = L.fromChunks <$> arbitrary
