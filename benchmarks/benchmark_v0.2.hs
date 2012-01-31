e8e0c8bbfcd2b4d1756a98a5e8f796c4ce9b5705
Felipe Lessa <felipe.lessa@gmail.com>
2012-01-31 10:16:33 +0000

    Fix warnings on benchmark_v0.2.hs

--- benchmarks/benchmark_v0.2.hs
+++ benchmarks/benchmark_v0.2.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Bio.Sequence.Stockholm
 import Bio.Sequence.Stockholm.Stream
-import Control.Monad.Exception.Synchronous (Exceptional(..))
 import System.IO (stdin)
 import qualified Data.Conduit as C
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as CL
 
+main :: IO ()
 main = main_event
 
+main_doc :: IO ()
 main_doc = C.runResourceT $ CB.sourceHandle stdin C.$$ parseStockholm C.=$ CL.mapM_ printName
     where
       printName (Stockholm file _ _) = print (findAnn AC file)
 
+main_event :: IO ()
 main_event = C.runResourceT $ CB.sourceHandle stdin C.$$ parseEvents C.=$ CL.mapM_ printName
     where
       printName (EvGF "AC" name) = print name
